405d328664310bc142edd0e5ccd32f89d8d765ab
tophsic <sicard.christophe@gmail.com>
2011-08-23 21:00:44 +0200

    Reload one file with callback function (mainly to test Reload) and ok test

--- src/reload.js
+++ src/reload.js
@@ -8,38 +8,30 @@
 (function(doc) {
    var reload_var = "reload";
    var api = window[reload_var] = function() {
-//      console.info('Reload JS');
       var args = arguments,
          fn = args[args.length - 1],
          els = {};
 
-         if (Object.prototype.toString.call(el) != '[object Function]')
+         if (Object.prototype.toString.call(fn) != '[object Function]')
             fn = null;
 
          var el = args[0];
-//         console.info(fn);
-//         console.info(el);
          if (el != fn) {
-//            el = getScript(el);
-//            els[el.name] = el;
-            scriptTag(el);
+            scriptTag(el, fn);
 
          }
    };
 
    function scriptTag(src, callback) {
-//      console.info('scriptTag');
       var s = doc.createElement('script');
       s.type = 'text/javascript';
       s.src = src;
- 
-//      console.info(s.state);
-
-//      s.onreadystatechange = s.onload = function () {
-//         console.info('onreadystatechange|onload');
-//         var state = s.readystate;
-//         console.info(state);
-//      }
+      s.onreadystatechange = s.onload = function () {
+         console.info('onreadystatechange|onload');
+         var state = s.readystate;
+         callback();
+         console.info(state);
+      }
       doc.body.appendChild(s);
    }
 })(document);
--- tests/reload.js
+++ tests/reload.js
@@ -3,9 +3,12 @@
  */
 
 module('Reload');
-
-test('1 file', 1, function () {
-    
+asyncTest('Without head async', 4, function() {
    ok(reload !== undefined, "reload");
- 
+   equal(typeof window.testVar, 'undefined', 'script not yet loaded');
+   reload('resources/scripttoreload.js', function() {
+      equal(typeof window.testVar, 'string', 'script loaded');
+      strictEqual(window.testVar, 'Hello World!', 'test var');
+      start();
+   });
 });
